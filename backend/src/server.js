34297378261c157b6baf3533211277912042992f
Ansh Prajapati <anshprajapati074@gmail.com>
2026-02-20 21:15:30 +0530

    Implemented MongoDB connection and integrated Clerk authentication in frontend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,22 @@
 import express from "express";
 import { ENV } from "./lib/env.js";
+import { connectDB } from "./lib/db.js";
 
 const app = express();
 
-console.log(ENV.PORT);
-
 app.get("/", (req, res) => {
-  res.status(200).json({ msg: "Success from api" });
+  res.status(200).json({ msg: "Success from API" });
 });
 
-app.listen(ENV.PORT, () => console.log("server is running on port:", ENV.PORT));
+const startServer = async () => {
+  try {
+    await connectDB();
+    app.listen(ENV.PORT, () => {
+      console.log("Server is running on port:", ENV.PORT);
+    });
+  } catch (error) {
+    console.error("Error while starting the server");
+  }
+};
+
+startServer();
