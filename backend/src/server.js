711d78e444cbb0b26f81924e5f5a2eae62ff6a2f
Ansh-3094 <anshprajapati074@gmail.com>
2026-02-22 12:06:39 +0530

    feat: integrate Inngest for user synchronization and deletion - Created a new user model - Updated environment variables to include Inngest and Stream API keys. - Configured CORS - Set up Inngest API routes in the server.

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,15 @@
 import express from "express";
 import { ENV } from "./lib/env.js";
 import { connectDB } from "./lib/db.js";
-
+import cors from "cors";
 const app = express();
+import { serve } from "inngest/express";
+import { inngest } from "./lib/inngest.js";
+
+app.use(express.json());
+app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
+
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
 app.get("/", (req, res) => {
   res.status(200).json({ msg: "Success from API" });
