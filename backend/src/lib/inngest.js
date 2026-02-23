2dd6b9854a89a9e1dd9a743a362ef80f2f255968
Ansh-3094 <anshprajapati074@gmail.com>
2026-02-23 18:40:40 +0530

    Bug fix

--- backend/src/lib/inngest.js
+++ backend/src/lib/inngest.js
@@ -6,31 +6,33 @@ export const inngest = new Inngest({ id: "interview-platform" });
 
 const syncUser = inngest.createFunction(
   { id: "sync-user" },
-  { event: "clerk / user.created" },
+  { event: "clerk.user.created" },
   async ({ event }) => {
     await connectDB();
 
-    const { id, email_address, first_name, last_name, image_url } = event.data;
+    const { id, email_addresses, first_name, last_name, image_url } =
+      event.data;
 
     const newUser = {
       clerkId: id,
-      email: email_address[0]?.email_address,
+      email: email_addresses[0]?.email_address,
       name: `${first_name || ""} ${last_name || ""}`,
       profileImage: image_url,
     };
 
-    await User.create(newUser);
+    const user = await User.create(newUser);
+    return { user };
   },
 );
 
 const deleteuserFromDB = inngest.createFunction(
   { id: "delete-user-from-db" },
-  { event: "clerk / user.deleted" },
+  { event: "clerk.user.deleted" },
   async ({ event }) => {
     await connectDB();
-
     const { id } = event.data;
     await User.deleteOne({ clerkId: id });
+    return { deleted: true };
   },
 );
 
