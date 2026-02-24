66cf30e1970612e8b681bd604695a38302a37149
Ansh-3094 <anshprajapati074@gmail.com>
2026-02-24 19:09:13 +0530

    Added stream-chat

--- backend/src/lib/inngest.js
+++ backend/src/lib/inngest.js
@@ -1,6 +1,7 @@
 import { Inngest } from "inngest";
 import { connectDB } from "./db.js";
 import User from "../models/user.model.js";
+import { deleteStreamUser, upsertStreamUser } from "./stream.js";
 
 export const inngest = new Inngest({ id: "interview-platform" });
 
@@ -21,6 +22,12 @@ const syncUser = inngest.createFunction(
     };
 
     const user = await User.create(newUser);
+
+    await upsertStreamUser({
+      id: newUser.clerkId.toString(),
+      name: newUser.name,
+      image: newUser.profileImage,
+    });
     return { user };
   },
 );
@@ -32,6 +39,8 @@ const deleteuserFromDB = inngest.createFunction(
     await connectDB();
     const { id } = event.data;
     await User.deleteOne({ clerkId: id });
+
+    await deleteStreamUser(id.toString());
     return { deleted: true };
   },
 );
