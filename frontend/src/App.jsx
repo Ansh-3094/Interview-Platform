e9bc20ee024d2cfbe5fa3b15b11c8627ceb58b92
Ansh-3094 <anshprajapati074@gmail.com>
2026-03-01 16:43:29 +0530

    Created Home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,13 +10,28 @@ import { Toaster } from "react-hot-toast";
 import { Routes, Route, Navigate } from "react-router";
 import HomePage from "./pages/HomePage";
 import ProblemsPage from "./pages/ProblemsPage";
+import DashboardPage from "./pages/DashboardPage";
 
 function App() {
-  const { isSignedIn } = useUser();
+  const { isSignedIn, isLoaded } = useUser();
+
+  //This will get rid of the Flickering Effect
+  if (!isLoaded) {
+    return null;
+  }
+
   return (
     <>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route
+          path="/"
+          element={!isSignedIn ? <HomePage /> : <Navigate to={"/dashboard"} />}
+        />
+
+        <Route
+          path="/dashboard"
+          element={isSignedIn ? <DashboardPage /> : <Navigate to={"/"} />}
+        />
 
         <Route
           path="/problems"
